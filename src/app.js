b150dbe67f7eca0481419a633b84d06f97d6edb6
Pedro Zaluar <pedrotrancosoz@gmail.com>
2020-06-21 23:27:10 -0300

    Inclusão dos tratamentos de teste

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const { uuid, isUuid } = require("uuidv4");
-const parseStringAsArray = require("./utils/parseStringAsArray");
 
 const app = express();
 
@@ -43,13 +42,9 @@ app.get('/repositories', (request, response) => {
 });
 
 app.post('/repositories', (request, response) => {
-  
   const { title, url, techs } = request.body;
-  //const numLike = 0;
-  
-  const techsArray = parseStringAsArray(techs); 
 
-  const repository = { id: uuid(), title, url, techs: techsArray, numLike: 0 };
+  const repository = { id: uuid(), title, url, techs, likes: 0 };
 
   repositories.push(repository);
   return response.status(200).json(repository);
@@ -60,36 +55,37 @@ app.put('/repositories/:id', (request, response) => {
   const { id } = request.params;
   const { title, url, techs } = request.body;
 
-  const repositoryIndex = repositories.findIndex(repositorie => repositorie.id == id);
+  const repositoryIndex = repositories.findIndex(repositoryIndex => repositoryIndex.id == id);
   
   if(repositoryIndex < 0){
     return response.status(400).json({message: 'Repositorie not found.'})
   }
 
-  const techsArray = parseStringAsArray(techs); 
+  let likes = repositories[repositoryIndex].likes;
 
-  const repositorie = {
-    id,
+  const repository = {
+    id: id,
     title,
     url,
-    techs: techsArray
+    techs,
+    likes: likes
   };
 
-  repositories[repositoryIndex] = repositorie;
+  repositories[repositoryIndex] = repository;
   
-  return response.json(repositorie);
+  return response.json(repository);
 });
 
 app.delete('/repositories/:id', (request, response) => {
   const { id } = request.params;
 
-  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id == id);
+  const repositoryIndex = repositories.find(repository => repository.id == id);
   
-  if(repositorieIndex < 0){
-    return response.status(400).json({message: 'Repositorie not found.'})
+  if(repositoryIndex < 0){
+    return response.status(400).send();
   }
   
-  repositories.splice(repositorieIndex, 1);
+  repositories.splice(repositoryIndex, 1);
   return response.status(204).send();
 });
 
@@ -102,7 +98,7 @@ app.post('/repositories/:id/like', (request, response) => {
     return response.status(400).json({message: 'Repositorie not found.'})
   }
     
-  repositories[repositoryIndex].numLike += 1;
+  repositories[repositoryIndex].likes += 1;
   
   return response.status(200).json(repositories[repositoryIndex]);
 });
