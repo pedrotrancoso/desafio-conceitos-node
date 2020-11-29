51fba13f7ef0eedeee54c111bd59b2620806021d
Pedro Zaluar <pedrotrancosoz@gmail.com>
2020-11-28 21:00:58 -0300

    Finalização do projeto de conceitos do nodejs

--- src/app.js
+++ src/app.js
@@ -55,20 +55,20 @@ app.put('/repositories/:id', (request, response) => {
   const { id } = request.params;
   const { title, url, techs } = request.body;
 
-  const repositoryIndex = repositories.findIndex(repositoryIndex => repositoryIndex.id == id);
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
   
   if(repositoryIndex < 0){
-    return response.status(400).json({message: 'Repositorie not found.'})
+    return response.status(400).json({error: 'Repositorie not found.'})
   }
 
   let likes = repositories[repositoryIndex].likes;
 
   const repository = {
-    id: id,
+    id,
     title,
     url,
     techs,
-    likes: likes
+    likes: repositories[repositoryIndex].likes
   };
 
   repositories[repositoryIndex] = repository;
@@ -79,32 +79,33 @@ app.put('/repositories/:id', (request, response) => {
 app.delete('/repositories/:id', (request, response) => {
   const { id } = request.params;
 
-  const repositoryIndex = repositories.find(repository => repository.id == id);
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
   
   if(repositoryIndex < 0){
-    return response.status(400).send();
+    return response.status(400).json({error: 'Repositorie not found.'});
+  }else{
+    repositories.splice(repositoryIndex, 1);
   }
   
-  repositories.splice(repositoryIndex, 1);
   return response.status(204).send();
 });
 
 app.post('/repositories/:id/like', (request, response) => {
   const { id } = request.params;
   
-  const repositoryIndex = repositories.findIndex(repositoryIndex => repositoryIndex.id == id);
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
   
   if(repositoryIndex < 0){
-    return response.status(400).json({message: 'Repositorie not found.'})
+    return response.status(400).json({error: 'Repositorie not found.'});
+  }else{
+    repositories[repositoryIndex].likes ++;
   }
-    
-  repositories[repositoryIndex].likes += 1;
   
-  return response.status(200).json(repositories[repositoryIndex]);
+  return response.json(repositories[repositoryIndex]);
 });
 
 //app.listen(3030, () => {
-//  console.log('😁 Back-end Started');
+//  console.log('?? Back-end Started');
 //});
 
 module.exports = app;
